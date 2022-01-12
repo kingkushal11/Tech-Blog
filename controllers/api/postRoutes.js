451177c21863d8a69792deab0c6eae208ba150a4
Kushal Khattri <kushalk62@gmail.com>
2022-01-11 17:29:25 -0800

    made a edit.js and conneted to post handlebar

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,6 +16,24 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.update(
+      req.body,{
+        where:{
+          user_id: req.session.user_id,
+          id: req.params.id
+        
+        }
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
---
+++ public/js/edit.js
@@ -0,0 +1,26 @@
+const editFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#post-name').value.trim();
+    const description = document.querySelector('#post-desc').value.trim();
+  
+    if (name && description) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/post');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+  
+  document
+  .querySelector('.edit-post-form')
+  .addEventListener('submit', editFormHandler);
